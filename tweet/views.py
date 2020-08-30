d243eeae803ceaa01cc001f8c92fbdc148e1417e
harika-daffy <harika.ch@capdomain.com>
2020-08-30 20:15:49 +0530

    home page listing page

--- tweet/views.py
+++ tweet/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse,Http404,JsonResponse
+from .models import Tweet
 # Create your views here.
+
+def home_view(request,*args,**kwargs):
+    print('lllllllllllllllllll')
+    print(args,kwargs)
+    return render(request,'pages/home.html',{})
+def tweet_detail_view(request,tweet_id,*args,**kwargs):
+    # return render(request,'listview.html',{})
+    data={
+        "id":tweet_id,
+    }
+    status=200
+    try:
+        obj=Tweet.objects.get(id=tweet_id)
+        data['content']=obj.content
+    except :
+        data['message']='page not found'
+        status=400    
+    return JsonResponse(data,status=status)
+    # return HttpResponse(f'list page {tweet_id}')
+def tweet_list_view(request,*args,**kwargs):
+    obj=Tweet.objects.all()
+    twee_list=[{ "id":a.id ,"content":a.content } for a in obj ]
+    data={
+        "resource":twee_list
+    }
+    return JsonResponse(data)
