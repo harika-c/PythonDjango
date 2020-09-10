2f067d1af28f0bf57c07f22b2fe75d3599e7fea7
harika-daffy <harika.ch@capdomain.com>
2020-09-10 06:57:22 +0530

    ngrok libraries got added

--- static/jsfiles/jsfile.js
+++ static/jsfiles/jsfile.js
@@ -1,19 +1,88 @@
-function clickMe(){
-birthday=prompt("what is you birth year ?")
-var days=(2020-birthday)*365
-var h1=document.createElement('h1')
-var node=document.createTextNode('your age is '+days)
+function rockGame(humanChoice)
+{
+console.log(humanChoice.id)
+var bot=botChoice(randomNumber())
+// console.log(bot)
+var a=returnArray(humanChoice.id,bot)
+info=whoWon(a)
+// console.log(info)
+frontEnd(humanChoice.id,bot,info)
+}
+function randomNumber(){
+    return Math.round(Math.random()*2)
+}
+function botChoice(number){
+    return ['rock','paper','scissors'][number]
+}
+function returnArray(humanChoice, botChoice){
+    choose={'rock':{'rock':0.5,'scissors':1,'paper':0},
+            'paper':{'rock':1,'scissors':0,'paper':0.5},
+            'scissors':{'rock':0,'scissors':0.5,'paper':1}
+    }
+    human=choose[humanChoice][botChoice]
+    bot=choose[botChoice][humanChoice]
+    
+    return [human,bot];
+}
+function whoWon([human,bot]){
+    if(human===0){
+        return {'message':'you lost','color':'red'}
+    }else if(human===0.5){
+        return {'message':'you tie','color':'yellow'}
+    }else{
+        return {'message':'you won','color':'green'}
+    }
 
-h1.setAttribute("id","ageInDays")
-h1.appendChild(node)
-document.getElementById('flex-box-result').appendChild(h1)
 }
-function reset(){
-    document.getElementById('ageInDays').remove()
-}
-function generatePika(){
-    var image=document.createElement('img')
-    image.src="https://media1.tenor.com/images/4c050b61abb46c8bc1fea2ecff97dc9f/tenor.gif?itemid=16139297"
-    document.getElementById('pika-generate').appendChild(image)
+function frontEnd(human,bot,info){
+    var data={
+        'rock':document.getElementById('rock').src,
+        'paper':document.getElementById('paper').src,
+        'scissors':document.getElementById('scissors').src
+    }
 
+    document.getElementById('rock').remove()
+    document.getElementById('paper').remove()
+    document.getElementById('scissors').remove()
+    
+    humanChoice=document.createElement('div')
+    bottChoice=document.createElement('div')
+    text=document.createElement('div')
+
+    humanChoice.innerHTML="<img src='"+data[human]+"' width=200 height=200 style='box-shadow:0px  10px 50px rgba(37,50,233,1)'>"
+    text.innerHTML="<h2 style='color:"+info.color+"'; font-size:80px; padding:30px;>"+info.message+"<h2>"
+    bottChoice.innerHTML="<img src='"+data[bot]+"' width=200 height=200 style='box-shadow:0px  10px 50px rgba(243,38,24,1)'>"
+    
+    console.log(humanChoice)    
+    
+    document.getElementById('fl').appendChild(humanChoice)
+    document.getElementById('fl').appendChild(text)
+    document.getElementById('fl').appendChild(bottChoice)
+    
 }
+buttons=document.getElementsByTagName("button")
+var copy=[]
+for (let i=0; i<buttons.length;i++)
+{
+    console.log(copy.push(buttons[i].classList[1]))
+}
+console.log(copy)
+
+function buttonColorChange(ele)
+{
+    console.log(ele.value)
+    if(ele.value === 'red'){
+        functionRed();
+    }else if(ele.value==='green'){
+        functionGreen();
+    }else if(ele.value ==='reset'){
+        functionReset();
+    }
+}
+function functionRed() {
+    for(let i=0;i<copy.length;i++){
+        buttons[i].classList.remove(copy[i].classList[1]);
+        buttons[i].classList.add('btn-danger');
+    }
+}
+    
