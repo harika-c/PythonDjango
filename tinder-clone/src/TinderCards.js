d654bee1d11327dce7c80353ab1cd06b401f1a81
harika-daffy <harika.ch@capdomain.com>
2020-10-05 21:09:14 +0530

    tinder-clone

--- tinder-clone/src/TinderCards.js
+++ tinder-clone/src/TinderCards.js
@@ -1,11 +1,19 @@
-import React ,{useState} from 'react'
+import React ,{useState , useEffect} from 'react'
 import TinderCard from 'react-tinder-card' 
 import "./TinderCards.css";
+import axios from "./axios";
 
 function TinderCards() {
-    const [people,setPeople]=useState([
-        
-    ])
+    const [people,setPeople]=useState([]);
+    useEffect(()=> {
+        async function fetchData(){
+            const req =await axios.get('/tinder/cards');
+            console.log('/////',req)
+            setPeople(req.data);
+        }
+        fetchData();
+    },[]);
+
     const swiped=(re,name)=>{
         console.log(re,name)
     }
@@ -27,7 +35,7 @@ function TinderCards() {
 
                     >
                         <div 
-                            style={{ backgroundImage: `url(${aa.image33})`}}
+                            style={{ backgroundImage: `url(${aa.imgUrl})`}}
                             className="card"
                         >
                         
